3121de922966107b4f2db2f6715446828089c82a
Eric Choi <emchoi91@gmail.com>
2023-06-24 21:06:57 -0700

    add tmux to configs

--- zsh/10-general.zsh
+++ zsh/10-general.zsh
@@ -1,3 +1,7 @@
+if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
+  exec tmux new-session -A -s main
+fi
+
 bindkey -v
 bindkey "^H" backward-delete-char
 bindkey "^?" backward-delete-char
