eced4918531890e2ff6bc37d77df27c24f4b2094
Eric Choi <emchoi91@gmail.com>
2023-06-25 16:30:24 -0700

    add fzf configs

--- zsh/10-general.zsh
+++ zsh/10-general.zsh
@@ -1,5 +1,5 @@
 if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
-  exec tmux new-session -A -s main
+    exec tmux new-session -A -s main
 fi
 
 bindkey -v
@@ -8,3 +8,10 @@ bindkey "^?" backward-delete-char
 
 alias vi=nvim
 alias vim=nvim
+
+if command -v fd &> /dev/null
+then
+    export FZF_DEFAULT_COMMAND="fd --type f --strip-cwd-prefix"
+    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+fi
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
