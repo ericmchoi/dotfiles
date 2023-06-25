90686b281abb2ac206e2045d9bb39c6e688da053
Eric Choi <emchoi91@gmail.com>
2023-06-24 21:07:41 -0700

    adjust nvim options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,6 +1,7 @@
-vim.opt.mouse = '' -- turn mouse off
+vim.opt.mouse = "" -- turn mouse off
 vim.opt.scrolloff = 4 -- lines of context
 vim.opt.wrap = false -- turn text wrap off
+vim.opt.termguicolors = true -- enable 24-bit RGB color
 
 -- turn on line numbers
 vim.opt.number = true
