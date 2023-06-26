272b067ac4ad8bc9e3a2ec8035e455cffe1b162b
Eric Choi <emchoi91@gmail.com>
2023-06-25 17:14:49 -0700

    fix antidote cloning into wrong directory

--- zsh/70-antidote.zsh
+++ zsh/70-antidote.zsh
@@ -1,6 +1,6 @@
 # Antidote plugin manager setup
 zstyle ':antidote:bundle' file $HOME/.config/zsh_plugins.txt
 [[ -d $ZSH_PLUGIN_PATH/antidote ]] || \
-  git clone https://github.com/mattmc3/antidote $ZSH_PLUGIN_PATH
+  git clone https://github.com/mattmc3/antidote $ZSH_PLUGIN_PATH/antidote
 source $ZSH_PLUGIN_PATH/antidote/antidote.zsh
 antidote load
