a58fd323716b004e279547feec4a38b1cfe05c81
Eric Choi <emchoi91@gmail.com>
2023-10-01 23:15:44 -0700

    simplify and improve prompt

--- zsh/10-general.zsh
+++ zsh/10-general.zsh
@@ -1,9 +1,12 @@
-bindkey -v
-bindkey "^H" backward-delete-char
-bindkey "^?" backward-delete-char
-
 alias vi=nvim
 alias vim=nvim
+
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+setopt PROMPT_SUBST
+PROMPT='%M:%F{blue}%~%f%F{yellow}$(parse_git_branch)%f'$'\n''$ ';
 
 if command -v fd &> /dev/null; then
     export FZF_DEFAULT_COMMAND="fd --type f --strip-cwd-prefix"
--- zsh/90-starship.zsh
+++
@@ -1,6 +0,0 @@
-# Starship prompt setup
-mkdir -p $ZSH_PLUGIN_PATH/starship
-path+=$ZSH_PLUGIN_PATH/starship
-command -v starship &> /dev/null || \
-  curl -sS https://starship.rs/install.sh | sh -s -- -y -b $ZSH_PLUGIN_PATH/starship
-eval "$(starship init zsh)"
