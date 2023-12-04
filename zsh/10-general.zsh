c724136d90af667724cf9d91323df0d2a4dbd924
Eric Choi <emchoi91@gmail.com>
2023-12-03 22:05:34 -0800

    fix zsh history fuzzy find

--- zsh/10-general.zsh
+++ zsh/10-general.zsh
@@ -12,4 +12,6 @@ if command -v fd &> /dev/null; then
     export FZF_DEFAULT_COMMAND="fd --type f --strip-cwd-prefix"
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 fi
-[[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
+
+# zsh-vi-mode fzf compatibility fix
+zvm_after_init_commands+=('[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh')
