d002099f77ab1ed6d340e641c45d9f4b0eb6b8f6
Eric Choi <emchoi91@gmail.com>
2024-10-05 14:30:26 -0700

    adjust nvim-cmp sources

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -4,6 +4,7 @@ return {
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
         "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
     },
     config = function()
@@ -37,6 +38,7 @@ return {
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
                 { name = "luasnip" },
+                { name = "path" },
                 { name = "buffer" },
             }),
         })
